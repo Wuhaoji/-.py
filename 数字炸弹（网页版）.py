af72b067ae9ad615405a6e10624785de0a5f1b99
Wuhaoji <166167220+Wuhaoji@users.noreply.github.com>
2024-06-28 02:48:16 +0800

    Add files via upload

--- 数字炸弹（网页版）.py
+++ 数字炸弹（网页版）.py
@@ -1,53 +1,67 @@
 import random
 import streamlit as st
 
-# 初始化全局变量
+# 初始化炸弹的数字
 if 'bomb' not in st.session_state:
     st.session_state.bomb = random.randint(1, 99)
+
+# 初始化范围
 if 'start' not in st.session_state:
     st.session_state.start = 0
 if 'end' not in st.session_state:
     st.session_state.end = 99
-if 'message' not in st.session_state:
-    st.session_state.message = ''
 
-st.title("猜数字游戏")
+# 初始化记录
+if 'history' not in st.session_state:
+    st.session_state.history = []
 
-st.write("请输入一个数字在{}到{}之间:".format(st.session_state.start, st.session_state.end))
+st.title("数字炸弹游戏")
 
-# 玩家输入
-people = st.number_input("Waoji的数字", min_value=st.session_state.start, max_value=st.session_state.end, step=1)
+# 显示当前范围和输入框
+st.write(f'请输入 {st.session_state.start} 到 {st.session_state.end} 之间的数:')
+people_input = st.text_input('你的输入:', '')
 
+# 按钮提交
 if st.button('提交'):
-    if people > st.session_state.bomb:
-        st.session_state.message = '大了'
-        st.session_state.end = people
-    elif people < st.session_state.bomb:
-        st.session_state.message = '小了'
-        st.session_state.start = people
-    else:
-        st.session_state.message = 'BOOM!!!'
-        st.session_state.bomb = random.randint(1, 99)
-        st.session_state.start = 0
-        st.session_state.end = 99
-    st.experimental_rerun()
+    try:
+        people = int(people_input)
+        if st.session_state.start < people < st.session_state.end:
+            st.session_state.history.append(f'你输入: {people}')
+            if people > st.session_state.bomb:
+                st.write('大了')
+                st.session_state.end = people
+            elif people < st.session_state.bomb:
+                st.write('小了')
+                st.session_state.start = people
+            else:
+                st.write('BOOM!!! 靓女赢了!')
+                st.session_state.history.append('靓女赢了!')
+                st.stop()  # 结束游戏
 
-st.write(st.session_state.message)
+            # 电脑输入
+            if st.session_state.start + 1 < st.session_state.end:
+                st.write(f'等待靓女输入 {st.session_state.start} 到 {st.session_state.end} 之间的数...')
+                com = random.randint(st.session_state.start + 1, st.session_state.end - 1)
+                st.write(f'靓女输入：{com}')
+                st.session_state.history.append(f'电脑输入: {com}')
+                if com > st.session_state.bomb:
+                    st.write('大了')
+                    st.session_state.end = com
+                elif com < st.session_state.bomb:
+                    st.write('小了')
+                    st.session_state.start = com
+                else:
+                    st.write('BOOM!!! you赢了!')
+                    st.session_state.history.append('you赢了!')
+                    st.stop()  # 结束游戏
 
-# 电脑输入
-if st.session_state.message != 'BOOM!!!':
-    st.write('等待靓女输入{}到{}之间的数...'.format(st.session_state.start, st.session_state.end))
-    com = random.randint(st.session_state.start + 1, st.session_state.end - 1)
-    st.write('靓女：{}'.format(com))
-    if com > st.session_state.bomb:
-        st.write('大了')
-        st.session_state.end = com
-    elif com < st.session_state.bomb:
-        st.write('小了')
-        st.session_state.start = com
-    else:
-        st.write('BOOM!!!')
-        st.session_state.bomb = random.randint(1, 99)
-        st.session_state.start = 0
-        st.session_state.end = 99
+            st.write(f'请输入 {st.session_state.start} 到 {st.session_state.end} 之间的数:')        
+        else:
+            st.write(f'请输入一个在 {st.session_state.start} 到 {st.session_state.end} 范围内的数')
+    except ValueError:
+        st.write('请输入有效的数字')
 
+# 显示历史记录
+st.write("历史记录:")
+for record in st.session_state.history:
+    st.write(record)
